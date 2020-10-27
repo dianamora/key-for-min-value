975e398df1610da8e9d0a54b5dec7eb8a0b34f33
Diana Mora <morra.dianna@gmail.com>
2020-10-27 11:00:46 -0400

    commit tree c4d8067f984a90bcb7ccb09a17431aa70583c76e

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    min_value = 0
+    empty_hash = nil
+    name_hash.each do |name, value|
+        if min_value == 0 || value < min_value
+            min_value = value
+            empty_hash = name
+        end 
+    end
+    empty_hash
 end
+
+
+   
